f94120c800f5ca71ade49be845fd0b596815eaaf
dingding51688 <dingding51688@gmail.com>
2026-04-05 21:54:07 +0100

    ui: remove footer from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { AuthProvider } from "@/components/providers/AuthProvider";
 import { CartProvider } from "@/components/providers/CartProvider";
 import { NotificationProvider } from "@/components/providers/NotificationProvider";
-import { DesktopHeader, Footer } from "@/components/navigation";
+import { DesktopHeader } from "@/components/navigation";
 
 export const dynamic = "force-dynamic";
 
@@ -18,13 +18,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="text-white">
         <AuthProvider>
           <CartProvider>
-          <NotificationProvider>
-            <div className="min-h-screen flex flex-col">
-              <DesktopHeader />
-              <main className="mx-auto w-full max-w-6xl flex-1 px-4 py-8">{children}</main>
-              <Footer />
-            </div>
-          </NotificationProvider>
+            <NotificationProvider>
+              <div className="min-h-screen flex flex-col">
+                <DesktopHeader />
+                <main className="mx-auto w-full max-w-6xl flex-1 px-4 py-8">{children}</main>
+              </div>
+            </NotificationProvider>
           </CartProvider>
         </AuthProvider>
       </body>
